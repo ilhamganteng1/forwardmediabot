1172290cf33e88e0d0dcef46ec1d87c83ed6ed2f
ilhamganteng1 <79833079+ilhamganteng1@users.noreply.github.com>
2025-02-12 14:00:19 +0700

    Update messages.py

--- forwardscoverbot/messages.py
+++ forwardscoverbot/messages.py
@@ -1,26 +1,44 @@
+# ForwardsCoverBot - don't let people on telegram forward with your name on the forward label
+# Copyright (C) 2017-2024  Dario <dariomsn@hotmail.it> (github.com/91DarioDev)
+#
+# ForwardsCoverBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ForwardsCoverBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with ForwardsCoverBot.  If not, see <http://www.gnu.org/licenses/>
+
+
 import time
 
-from telegram import ParseMode
-from telegram.ext import DispatcherHandlerStop
+from telegram.constants import ParseMode
+from telegram.ext import ApplicationHandlerStop, Application
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 from forwardscoverbot import dbwrapper
 from forwardscoverbot import keyboards
 
-from telegram.ext.dispatcher import run_async
 
-
-def before_processing(update, context):
+async def before_processing(update, context):
     if update.effective_chat.type != "private":
         text = "This bot can be used only in private chats! I leave! Bye!"
         keyboard = keyboards.private_chat_kb()
-        update.effective_message.reply_text(text=text, reply_markup=keyboard)
-        context.bot.leave_chat(chat_id=update.effective_message.chat_id)
-        raise DispatcherHandlerStop
+        await update.effective_message.reply_text(text=text, reply_markup=keyboard)
+        await context.bot.leave_chat(chat_id=update.effective_message.chat_id)
+        raise ApplicationHandlerStop
         
     else:
         int_time = int(time.mktime(update.effective_message.date.timetuple()))
-        context.dispatcher.run_async(dbwrapper.add_user_db, update.effective_message.from_user.id, int_time)
+        context.application.create_task(
+            dbwrapper.add_user_db(update.effective_message.from_user.id, int_time),
+            update=update
+        )
 
 
 def get_message_reply_markup_inline_keyboard(message):
@@ -42,9 +60,10 @@ def leave_only_url_buttons_in_reply_markup(inline_keyboard):
 
 
 
-def process_message(
+async def process_message(
         update, context, message=None, remove_caption=False, custom_caption=None, 
-        remove_buttons=False, custom_reply_markup=None, disable_web_page_preview=False):
+        remove_buttons=False, custom_reply_markup=None, disable_web_page_preview=False,
+        spoiler_action=None):
 
     if not message:
         message = update.effective_message
@@ -64,7 +83,7 @@ def process_message(
         inline_keyboard, removed_buttons_from_keyboard = leave_only_url_buttons_in_reply_markup(keyboard_not_cleaned)
         reply_markup = InlineKeyboardMarkup(inline_keyboard)
         if len(removed_buttons_from_keyboard) > 0:
-            message.reply_text(
+            await message.reply_text(
                 '{} buttons have been removed. I support only link buttons'.format(
                     len(removed_buttons_from_keyboard)
                 )
@@ -73,18 +92,26 @@ def process_message(
         reply_markup = None
 
 
+    new_spoiler = message.has_media_spoiler
+    if spoiler_action:
+        if spoiler_action == 'add':
+            new_spoiler = True
+        if spoiler_action == 'remove':
+            new_spoiler = False
+
+
     if message.text:
-        message.reply_text(
+        await message.reply_text(
             text=message.text_html, 
             parse_mode=ParseMode.HTML, 
             reply_markup=reply_markup,
-            disable_web_page_preview=disable_web_page_preview
+            link_preview_options=message.link_preview_options if not disable_web_page_preview else {'is_disabled': True}
         )
 
     elif message.voice:
         media = message.voice.file_id
         duration = message.voice.duration
-        message.reply_voice(
+        await message.reply_voice(
             voice=media, 
             duration=duration, 
             caption=caption, 
@@ -94,16 +121,29 @@ def process_message(
 
     elif message.photo:
         media = message.photo[-1].file_id
-        message.reply_photo(
+        await message.reply_photo(
             photo=media, 
             caption=caption, 
             parse_mode=ParseMode.HTML, 
-            reply_markup=reply_markup
+            reply_markup=reply_markup,
+            has_spoiler=new_spoiler,
+            show_caption_above_media=message.show_caption_above_media
+        )
+
+    elif message.animation:
+        media = message.animation.file_id
+        await message.reply_animation(
+            animation=media, 
+            caption=caption, 
+            parse_mode=ParseMode.HTML, 
+            reply_markup=reply_markup,
+            has_spoiler=new_spoiler,
+            show_caption_above_media=message.show_caption_above_media
         )
 
     elif message.sticker:
         media = message.sticker.file_id
-        message.reply_sticker(
+        await message.reply_sticker(
             sticker=media, 
             reply_markup=reply_markup
         )
@@ -111,7 +151,7 @@ def process_message(
     elif message.document:
         media = message.document.file_id
         filename = message.document.file_name
-        message.reply_document(
+        await message.reply_document(
             document=media, 
             filename=filename, 
             caption=caption, 
@@ -124,7 +164,7 @@ def process_message(
         duration = message.audio.duration
         performer = message.audio.performer
         title = message.audio.title
-        message.reply_audio(
+        await message.reply_audio(
             audio=media, 
             duration=duration, 
             performer=performer, 
@@ -137,19 +177,21 @@ def process_message(
     elif message.video:
         media = message.video.file_id
         duration = message.video.duration
-        message.reply_video(
+        await message.reply_video(
             video=media, 
             duration=duration, 
             caption=caption, 
             parse_mode=ParseMode.HTML, 
-            reply_markup=reply_markup
+            reply_markup=reply_markup,
+            has_spoiler=new_spoiler,
+            show_caption_above_media=message.show_caption_above_media
         )
 
     elif message.contact:
         phone_number = message.contact.phone_number
         first_name = message.contact.first_name
         last_name = message.contact.last_name
-        message.reply_contact(
+        await message.reply_contact(
             phone_number=phone_number, 
             first_name=first_name, 
             last_name=last_name, 
@@ -162,7 +204,7 @@ def process_message(
         title = message.venue.title
         address = message.venue.address
         foursquare_id = message.venue.foursquare_id
-        message.reply_venue(
+        await message.reply_venue(
             longitude=longitude, 
             latitude=latitude, 
             title=title, 
@@ -174,21 +216,21 @@ def process_message(
     elif message.location:
         longitude = message.location.longitude
         latitude = message.location.latitude
-        message.reply_location(latitude=latitude, longitude=longitude, reply_markup=reply_markup)
+        await message.reply_location(latitude=latitude, longitude=longitude, reply_markup=reply_markup)
 
     elif message.video_note:
         media = message.video_note.file_id
         length = message.video_note.length
         duration = message.video_note.duration
-        message.reply_video_note(video_note=media, length=length, duration=duration, reply_markup=reply_markup)
+        await message.reply_video_note(video_note=media, length=length, duration=duration, reply_markup=reply_markup)
     
     elif message.dice:
-        context.bot.sendDice(chat_id=update.effective_user.id, reply_markup=reply_markup, emoji=message.dice.emoji)
+        await context.bot.sendDice(chat_id=update.effective_user.id, reply_markup=reply_markup, emoji=message.dice.emoji)
 
     elif message.game:
         text = "Sorry, telegram doesn't allow to echo this message"
-        message.reply_text(text=text, quote=True)
+        await message.reply_text(text=text, quote=True)
 
     else:
         text = "Sorry, this kind of media is not supported yet"
-        message.reply_text(text=text, quote=True)
+        await message.reply_text(text=text, quote=True)
